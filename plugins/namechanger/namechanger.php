d4536bfafa213ed7e4d3ef9d04856bb3948ee3e2
alex-hrytsenko <alex.hrytsenko1@gmail.com>
2022-08-18 14:24:21 +0300

    added basic functionality with counter

--- plugins/namechanger/namechanger.php
+++ plugins/namechanger/namechanger.php
@@ -13,33 +13,39 @@ define('NAMECHANGER_DIR', plugin_dir_path(__FILE__));
 
 function name_changer_filter($content) {
 	static $names = [];
+	$content_arr = explode(' ', $content);
+	$people_arr = [];
+	$counter = 1;
+
 	if(empty($names)) {
 		$names = explode(',', file_get_contents(NAMECHANGER_DIR . 'list_of_names.txt'));
 	}
-	print_r(explode(' ', $content));
-	// 2------------------------------------------------
-	$content_arr = explode(' ', $content);
 	for($i = 0; $i < count($content_arr); $i++)
 	{
 		for($j = 0; $j < count($names); $j++)
 		{
 			if($content_arr[$i] === $names[$j])
 			{
-				$content_arr[$i] = 'xxx';
+				$full_name = $content_arr[$i-1] . ' ' . $content_arr[$i] . ' ' . $content_arr[$i+1];
+				if(!array_key_exists($full_name, $people_arr))
+				{
+					$people_arr[$full_name] = $counter;
+					//replace first name
+					$content_arr[$i] = 'ОСОБА_' . $counter++;
+				} else
+				{
+					//replace first name
+					$content_arr[$i] = 'ОСОБА_' . $people_arr[$full_name];
+				}
+				//delete last and middle name
 				array_splice($content_arr, $i-1, 1);
 				array_splice($content_arr, $i, 1);
-				//unset($content_arr[$i+1]);
 			}
 		}
 	}
-	print_r($content_arr);
+
+	$content = implode(' ', $content_arr);
 	return $content;
 }
 
 add_filter('the_content', 'name_changer_filter');
-
-/*
-	for($i = 0; $i < count($names); $i++) {
-		//$content = preg_replace('#' . $names[$i] . '#iu', 'ОСОБА_1', $content);
-	}
-*/
